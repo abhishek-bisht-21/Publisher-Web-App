38bee8f64f38dbe61885873e8e64610d8b613862
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2024-06-02 19:08:21 +0530

    Added env variable and mongo connection string

--- app/app.js
+++ app/app.js
@@ -1,5 +1,9 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import dbConnect from '../config/dbConnect.js';
 
+dbConnect();
 const app = express();
 
 
--- config/dbConnect.js
+++ config/dbConnect.js
@@ -4,7 +4,8 @@ const dbConnect = async () => {
   try {
     mongoose.set("strictQuery", false);
     const connected = await mongoose.connect(process.env.MONGO_URL);
-    console.log(`Mongodb connected ${connected.connection.host}`);
+    mongoose.set('strictQuery', false);
+    console.log(`Mongodb connected ${(await connected).connection.host} `);
   } catch (error) {
     console.log(`Error: ${error.message}`);
     process.exit(1);
@@ -12,3 +13,7 @@ const dbConnect = async () => {
 };
 
 export default dbConnect;
+
+// uS2DmQEFnO5YZiKN
+
+// mongodb+srv://abhishekbisht21:<password>@cluster0.xptyoou.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
