54e766f59713304363136f614066a884e18d8c5d
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2024-06-02 18:50:34 +0530

    Updated User Schema and add dbConnect

--- model/User.js
+++ model/User.js
@@ -22,13 +22,49 @@ const UserSchema = new Schema({
 		},
 	],
 
+	wishLists: [
+
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "WishList"
+		},
+	],
+
 	isAdmin:
 	{
 		type: Boolean,
 		default: false
 	},
-
-
+	hasShippingAddress: {
+      type: Boolean,
+      default: false,
+    },
+    shippingAddress: {
+      firstName: {
+        type: String,
+      },
+      lastName: {
+        type: String,
+      },
+      address: {
+        type: String,
+      },
+      city: {
+        type: String,
+      },
+      postalCode: {
+        type: String,
+      },
+      province: {
+        type: String,
+      },
+      country: {
+        type: String,
+      },
+      phone: {
+        type: String,
+      },
+    }
 }, {
 	timestamps: true,
 });
