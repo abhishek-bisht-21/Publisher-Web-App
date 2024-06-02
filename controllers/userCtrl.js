a8fd661596d09daaaf078251883087d4ebfc8236
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2024-06-03 02:39:02 +0530

    Authorization: Added logic to fetch jwt token from header

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -2,7 +2,7 @@ import User from "../model/User.js";
 import bcrypt from 'bcryptjs';
 import asyncHandler from "express-async-handler";
 import generateToken from "../utils/generateToken.js";
-
+import { getTokenFromHeader } from "../utils/getTokenFromHeader.js";
 
 // @desc    Register user
 // @route   POST /api/v1/users/register
@@ -65,7 +65,10 @@ export const loginUserCtrl = asyncHandler(async (req, res) => {
 // @route   GET /api/v1/users/profile
 // @access  Private
 export const getUserProfileCtrl = asyncHandler(async (req, res) => {
-  res.json({
-    msg: "Welcome profile page",
-  });
+	// get token from header
+	const token = getTokenFromHeader(req);
+	console.log(token);
+	res.json({
+		msg: "Welcome profile page",
+	});
 });
