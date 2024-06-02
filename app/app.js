9ea0f2b9f37f1af9d5d94585435e167878d69145
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2024-06-02 23:29:40 +0530

    user controller setup

--- app/app.js
+++ app/app.js
@@ -7,6 +7,9 @@ import userRoutes from '../routes/usersRoute.js';
 dbConnect();
 const app = express();
 
+//parse incoming data
+app.use(express.json());
+
 //routes
 app.use('/', userRoutes);
 
--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -2,9 +2,36 @@ import User from "../model/User.js";
 
 
 
+// @desc    Register user
+// @route   POST /api/v1/users/register
+// @access  Private/Admin
+export const registerUserCtrl = async (req, res) => {
+	
+	const { fullname, email, password } = req.body;
 
-export const registerUserCtrl  = async(req, res) => {
-	res.json({
-		msg: "User register controller"
+	// check user exists
+	const userExists = await User.findOne({ email });
+
+	if (userExists) {
+		//throw
+		res.json({
+			msg: "User already exists",
+		});
+	}
+
+	// hash password
+	// create the user
+	const user = await User.create({
+		fullname,
+		email, 
+		password
 	});
+
+
+	res.status(201).json({
+		status: 'success',
+		message: 'user registered succesfully',
+		data: user
+	});
+
 };
