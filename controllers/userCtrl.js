86c3e37d97577a2b51847d26db1b33936c5ca0d8
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2024-06-02 23:33:40 +0530

    secured password by hashing it using bcrypt

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,12 +1,12 @@
 import User from "../model/User.js";
-
+import bcrypt from 'bcryptjs';
 
 
 // @desc    Register user
 // @route   POST /api/v1/users/register
 // @access  Private/Admin
 export const registerUserCtrl = async (req, res) => {
-	
+
 	const { fullname, email, password } = req.body;
 
 	// check user exists
@@ -20,11 +20,14 @@ export const registerUserCtrl = async (req, res) => {
 	}
 
 	// hash password
+	const salt = await bcrypt.genSalt(10);
+	const hashedPassword = await bcrypt.hash(password, salt);
+
 	// create the user
 	const user = await User.create({
 		fullname,
 		email, 
-		password
+		password: hashedPassword
 	});
 
 
