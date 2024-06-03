421e939d676b86f1f7024a40ca00b429796f4bca
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2024-06-03 09:56:33 +0530

    Get all products implemented

--- routes/productsRoute.js
+++ routes/productsRoute.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { createProductCtrl } from '../controllers/productCtrl.js';
+import { createProductCtrl, getProductsCtrl } from '../controllers/productCtrl.js';
 import { isLoggedIn } from '../middlewares/isLoggedIn.js';
 
 const productsRouter = express.Router();
 
 
 productsRouter.post('/', isLoggedIn, createProductCtrl);
+productsRouter.get('/', getProductsCtrl);
 
 export default productsRouter;
