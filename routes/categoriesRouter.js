cbe10f3db6e93fb0803fbd47c3a0b5b17f11db5e
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2024-06-08 21:11:10 +0530

    Categories Get Get All Update Delete impplemented

--- routes/categoriesRouter.js
+++ routes/categoriesRouter.js
@@ -1,9 +1,14 @@
 import express  from "express";
-import { createCategoryCtrl } from "../controllers/categoriesCtrl.js";
+import { createCategoryCtrl, deleteCategoryCtrl, getAllCategoriesCtrl, getSingleCategoryCtrl, updateCategoryCtrl } from "../controllers/categoriesCtrl.js";
 import { isLoggedIn } from '../middlewares/isLoggedIn.js';
+
 
 const categoriesRouter = express.Router();
 
 categoriesRouter.post("/", isLoggedIn,createCategoryCtrl);
+categoriesRouter.get("/", getAllCategoriesCtrl);
+categoriesRouter.get("/:id", getSingleCategoryCtrl);
+categoriesRouter.delete("/:id", deleteCategoryCtrl);
+categoriesRouter.put("/:id", updateCategoryCtrl);
 
 export default categoriesRouter
