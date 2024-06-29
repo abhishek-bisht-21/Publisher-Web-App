f90a9516d5370dee06cd71a7ebd2fb0df3e38e8c
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2024-06-29 12:26:41 +0530

    implemented stripe webhook logic in app.js

--- app/app.js
+++ app/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import Stripe from "stripe";
 dotenv.config();
 import dbConnect from '../config/dbConnect.js';
 import userRoutes from '../routes/usersRoute.js';
@@ -10,10 +11,65 @@ import brandsRouter from '../routes/brandsRouter.js';
 import colorRouter from '../routes/colorRouter.js';
 import reviewRouter from '../routes/reviewsRouter.js';
 import orderRouter from '../routes/ordersRouter.js';
+import Order from "../model/Order.js";
 
 
 dbConnect();
 const app = express();
+
+//Stripe webhook
+//stripe instance
+const stripe = new Stripe(process.env.STRIPE_KEY);
+
+// This is your Stripe CLI webhook secret for testing your endpoint locally.
+const endpointSecret =
+  "whsec_52e03d91547d4a90dfb03f811f9650aa17ef0b17cd4ffa699ff4447d0c4a0265";
+
+app.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  async (request, response) => {
+    const sig = request.headers["stripe-signature"];
+
+    let event;
+
+    try {
+      event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+      console.log("event");
+    } catch (err) {
+      console.log("err", err.message);
+      response.status(400).send(`Webhook Error: ${err.message}`);
+      return;
+    }
+    if (event.type === "checkout.session.completed") {
+      //update the order
+      const session = event.data.object;
+      const { orderId } = session.metadata;
+      const paymentStatus = session.payment_status;
+      const paymentMethod = session.payment_method_types[0];
+      const totalAmount = session.amount_total;
+      const currency = session.currency;
+      //find the order
+      const order = await Order.findByIdAndUpdate(
+        JSON.parse(orderId),
+        {
+          totalPrice: totalAmount / 100,
+          currency,
+          paymentMethod,
+          paymentStatus,
+        },
+        {
+          new: true,
+        }
+      );
+      console.log(order);
+    } else {
+      return;
+    }
+    response.send();
+  }
+);
+
 
 //parse incoming data
 app.use(express.json());
