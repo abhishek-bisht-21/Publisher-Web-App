d0515e558f49b9bc4d8ec9e1fdaf2bf303d348b1
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2024-07-02 00:13:26 +0530

    Added config file for fileUpload

--- routes/productsRoute.js
+++ routes/productsRoute.js
@@ -3,13 +3,15 @@ import { createProductCtrl, getProductCtrl, getProductsCtrl, updateProductCtrl, 
 import { isLoggedIn } from '../middlewares/isLoggedIn.js';
 import isAdmin from '../middlewares/isAdmin.js';
 
+import upload from "../config/fileUpload.js";
+
 const productsRouter = express.Router();
 
 
-productsRouter.post('/', isLoggedIn, createProductCtrl);
+productsRouter.post('/', isLoggedIn,   isAdmin, upload.array("files"),createProductCtrl);
 productsRouter.get('/', getProductsCtrl);
 productsRouter.get('/:id',getProductCtrl);
-productsRouter.put('/:id',isLoggedIn ,updateProductCtrl);
-productsRouter.delete("/:id/delete", isLoggedIn, deleteProductCtrl);
+productsRouter.put('/:id',isLoggedIn ,isAdmin ,updateProductCtrl);
+productsRouter.delete("/:id/delete", isLoggedIn,isAdmin ,deleteProductCtrl);
 
 export default productsRouter;
